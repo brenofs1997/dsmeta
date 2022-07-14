89a275d50fd4adf585136deca83538d8762ddb09
PauloFH99 <brenofs1997@gmail.com>
2022-07-14 19:45:41 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import NotificationButton from './components/NotificationButton'
 
 
 function App() {
-  return(
-    <h1>Ola</h1>
+  return (
+    <>
+      <h1>Ola</h1>
+      <NotificationButton />
+    </>
   )
 }
 
