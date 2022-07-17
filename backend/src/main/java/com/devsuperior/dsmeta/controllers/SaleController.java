37a29b888e80d2eaa513b089df2faa1a420e9c82
PauloFH99 <brenofs1997@gmail.com>
2022-07-17 16:24:03 -0300

    API test

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,8 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
+	
+	@GetMapping
 	public List<Sale> findSales(){
 		return service.findSales();
 	}
